d7dd6a243b5df4238c0e882f7e64c15c0d09b4c3
Konrad Guzek <konrad@guzek.uk>
2024-10-12 16:25:23 +0200

    added splash page

--- twist/app/(main)/page.tsx
+++ twist/app/(main)/page.tsx
@@ -1,8 +1,36 @@
 export default function Home() {
-    return (
-      <div className='w-full h-screen flex items-center justify-center'>
-          <h1>homepage</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col">
+      <h1>Welcome to TwIST: Where Innovation Meets Opportunity</h1>
+      <p>
+        Unlock your future in tech with the next generation of job matchmaking.
+        At TechElevate, we don&apos;t just find you jobs—we engineer your
+        success. Powered by cutting-edge AI, our platform seamlessly pairs your
+        unique skills, ambitions, and experiences with exclusive IT
+        opportunities that align with your career aspirations.
+      </p>
+      <h3>Why TwIST?</h3>
+      <ul>
+        <li>
+          <b>Precision Matching:</b> Our advanced algorithms dive deep into your
+          profile, mapping your expertise to positions at leading global
+          companies.
+        </li>
+        <li>
+          <b>Tailored Opportunities:</b> Forget sifting through countless
+          listings. We bring curated opportunities directly to you, saving you
+          time and effort.
+        </li>
+        <li>
+          <b>Real-Time Insights:</b> Stay ahead of the curve with instant alerts
+          for positions that perfectly match your evolving career goals.
+        </li>
+        <li>
+          <b>High-Level Connections:</b> From startups to Fortune 500s, access
+          roles that aren&apos;t on the traditional job boards—positions
+          designed for innovators like you.
+        </li>
+      </ul>
+    </div>
+  );
+}
