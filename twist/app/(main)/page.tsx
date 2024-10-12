8b998631cdbcf87c4881dc4934dbdc89b25831a5
Konrad Guzek <konrad@guzek.uk>
2024-10-12 18:26:20 +0200

    style home page

--- twist/app/(main)/page.tsx
+++ twist/app/(main)/page.tsx
@@ -1,38 +1,41 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col">
-      <h1 className="text-2xl font-bold">
+      <h1 className="text-7xl font-bold">
         Welcome to TwIST: Where Innovation Meets Opportunity
       </h1>
-      <p>
-        Unlock your future in tech with the next generation of job matchmaking.
-        At TwIST, we don&apos;t just find you jobs—we engineer your success.
-        Powered by cutting-edge AI, our platform seamlessly pairs your unique
-        skills, ambitions, and experiences with exclusive IT opportunities that
-        align with your career aspirations.
-      </p>
-      <h3 className="text-xl font-bold">Why TwIST?</h3>
-      <ul>
-        <li>
-          <b>Precision Matching:</b> Our advanced algorithms dive deep into your
-          profile, mapping your expertise to positions at leading global
-          companies.
-        </li>
-        <li>
-          <b>Tailored Opportunities:</b> Forget sifting through countless
-          listings. We bring curated opportunities directly to you, saving you
-          time and effort.
-        </li>
-        <li>
-          <b>Real-Time Insights:</b> Stay ahead of the curve with instant alerts
-          for positions that perfectly match your evolving career goals.
-        </li>
-        <li>
-          <b>High-Level Connections:</b> From startups to Fortune 500s, access
-          roles that aren&apos;t on the traditional job boards—positions
-          designed for innovators like you.
-        </li>
-      </ul>
+      <div className="max-w-1/2 mt-4">
+        <p className="text-3xl">
+          Unlock your future in tech with the next generation of job
+          matchmaking. At TwIST, we don&apos;t just find you jobs—we engineer
+          your success. Powered by cutting-edge AI, our platform seamlessly
+          pairs your unique skills, ambitions, and experiences with exclusive IT
+          opportunities that align with your career aspirations.
+        </p>
+        <h3 className="text-5xl font-bold mt-20">Why TwIST?</h3>
+        <ul className="list-disc list-inside mt-4 flex flex-col gap-1.5 text-2xl">
+          <li>
+            <b>Precision Matching:</b> Our advanced algorithms dive deep into
+            your profile, mapping your expertise to positions at leading global
+            companies.
+          </li>
+          <li>
+            <b>Tailored Opportunities:</b> Forget sifting through countless
+            listings. We bring curated opportunities directly to you, saving you
+            time and effort.
+          </li>
+          <li>
+            <b>Real-Time Insights:</b> Stay ahead of the curve with instant
+            alerts for positions that perfectly match your evolving career
+            goals.
+          </li>
+          <li>
+            <b>High-Level Connections:</b> From startups to Fortune 500s, access
+            roles that aren&apos;t on the traditional job boards—positions
+            designed for innovators like you.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
