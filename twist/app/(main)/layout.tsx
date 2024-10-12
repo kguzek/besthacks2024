e405da61d86feb3d2d379d682c9e0c9dd9295aa7
michalges <105984108+michalges@users.noreply.github.com>
2024-10-12 16:25:05 +0200

    ...

--- twist/app/(main)/layout.tsx
+++ twist/app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { SignedIn, SignedOut } from '@/components/auth/session-manager'
 import UserButton from '@/components/auth/user-button'
+import { ModeToggle } from '@/components/theme-toggle'
 
 export default function DashboardLayout({
     children,
@@ -13,24 +14,37 @@ export default function DashboardLayout({
   }) {
   return (
     <>
-        <nav className='w-full fixed top-0 left-0'>
+        <nav className='w-full fixed top-0 left-0 z-50 border-b border-border'>
             <div className='w-11/12 flex items-center container mx-auto'>
                 {/* <Icons.Logo /> */}
-                <Image 
-                    src="/images/logo.png" 
-                    alt="logos" 
-                    width={512} 
-                    height={512} 
-                    className="w-32 h-auto"
-                />
+                <Link href="/" className=''>
+                    <Image  
+                        src="/images/logo-dark.png" 
+                        alt="logos" 
+                        width={512} 
+                        height={512} 
+                        className="w-28 h-auto dark:hidden"
+                    />
+                    <Image  
+                        src="/images/logo-light.png" 
+                        alt="logos" 
+                        width={512} 
+                        height={512} 
+                        className="w-28 h-auto hidden dark:block"
+                    />
+                </Link>
+
                 <Link href={"#"} className='p-6'>Home</Link>
                 <Link href={"#"} className='p-6'>O nas</Link>
                 <Link href={"#"} className='p-6'>Kontakt</Link>
+
+                <ModeToggle />
+                
                 <SignedIn>
-                    <UserButton className='ml-auto'/>
+                    <UserButton className=''/>
                 </SignedIn>
                 <SignedOut>
-                    <Link href={"/auth/login"} className='ml-auto'><Button className='ml-auto'>Login</Button></Link>
+                    <Link href={"/auth/login"} className='ml-4'><Button className=''>Login</Button></Link>
                 </SignedOut>
             </div>            
         </nav>
