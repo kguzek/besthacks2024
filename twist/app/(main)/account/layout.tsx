6cc6e12bbd1f228f4ef2a7098783480407aeaf59
Kamil Marczak <kamilm@you2.pl>
2024-10-12 17:42:07 +0200

    update

--- twist/app/(main)/account/layout.tsx
+++ twist/app/(main)/account/layout.tsx
@@ -2,23 +2,6 @@ import AccountSidebar from './_components/AccountSidebar';
 
 export default function DashboardLayout({
     children,
-<<<<<<< Updated upstream
-  }: {
-    children: React.ReactNode
-  }) {
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-        <div className="h-full w-1/5 flex flex-col justify-center border-r border-border">
-            <Button className='m-2 justify-start'>Twoje dane</Button>
-            <Button className='m-2 justify-start' variant="ghost">Bezpieczeństwo</Button>
-            <Button className='m-2 justify-start' variant="ghost">Statystyki</Button>
-            <Button className='m-2 justify-start' variant="ghost">Wyloguj</Button>
-        </div>        
-
-        {children}
-    </div>
-  )
-=======
 }: {
     children: React.ReactNode;
 }) {
@@ -28,5 +11,4 @@ export default function DashboardLayout({
             {children}
         </div>
     );
->>>>>>> Stashed changes
 }
