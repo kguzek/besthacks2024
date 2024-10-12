afe82ed4b798656c7cdd47cb01dc4f1617dfc9a5
michalges <105984108+michalges@users.noreply.github.com>
2024-10-12 21:48:50 +0200

    ...

--- twist/app/(main)/auth/register/page.tsx
+++ twist/app/(main)/auth/register/page.tsx
@@ -22,11 +22,20 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { signUpSchema } from '@/schemas';
 import { useTransition } from 'react';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription, } from '@/components/ui/form';
 import { registerUser } from '@/actions/profile';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import { UserRole } from '@prisma/client';
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 export default function RegisterForm() {
     const [isPending, startTransition] = useTransition()
@@ -149,6 +158,50 @@ export default function RegisterForm() {
                                                     <Input {...field} disabled={isPending} placeholder='********' type='password' autoComplete='new-password' />
                                                 </FormControl>
                                                 <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="dob"
+                                        render={({ field }) => (
+                                            <FormItem className="flex flex-col">
+                                            <FormLabel>Data urodzenia</FormLabel>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                    variant={"outline"}
+                                                    className={cn(
+                                                        "w-[240px] pl-3 text-left font-normal",
+                                                        !field.value && "text-muted-foreground"
+                                                    )}
+                                                    >
+                                                    {field.value ? (
+                                                        format(field.value, "PPP")
+                                                    ) : (
+                                                        <span>Pick a date</span>
+                                                    )}
+                                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    disabled={(date) =>
+                                                    date > new Date() || date < new Date("1900-01-01")
+                                                    }
+                                                    initialFocus
+                                                />
+                                                </PopoverContent>
+                                            </Popover>
+                                            <FormDescription>
+                                                Your date of birth is used to calculate your age.
+                                            </FormDescription>
+                                            <FormMessage />
                                             </FormItem>
                                         )}
                                     />
