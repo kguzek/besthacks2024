fa9ced5e9a3654b68e74603a13acae8d64f21233
Kamil Marczak <kamilm@you2.pl>
2024-10-12 18:51:45 +0200

    update

--- twist/app/(main)/account/_components/AccountSidebar.tsx
+++ twist/app/(main)/account/_components/AccountSidebar.tsx
@@ -1,16 +1,33 @@
 "use client"
 
+import { Icons } from '@/components/icons'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function AccountSidebar() {
     return (
         <div className="h-full w-1/5 flex flex-col border-r border-border">
-            <div className='pt-24 flex flex-col gap-1 pr-4'>
-                <Button className='justify-start'>Twoje dane</Button>
-                <Button className='justify-start' variant="ghost">Bezpieczeństwo</Button>
-                <Button className='justify-start' variant="ghost">Statystyki</Button>
-                <Button className='justify-start' variant="ghost">Wyloguj</Button>
+            <div className='pt-24 flex flex-col flex-1 gap-1 pr-4 pb-5'>
+                <Button className='justify-start'>
+                    <Icons.Profile className="w-4 h-4 mr-2" />Twoje dane
+                </Button>
+                <Button className='justify-start' variant="ghost">
+                    <Icons.Safe className="w-4 h-4 mr-2" />
+                    Bezpieczeństwo
+                </Button>
+                <Button className='justify-start' variant="ghost">
+                    <Icons.Stats className="w-4 h-4 mr-2" />
+                    Statystyki
+                </Button>
+                <Button className='justify-start' variant="ghost">
+                    <Icons.Settings2 className="w-4 h-4 mr-2" />
+                    Ustawienia
+                </Button>
+                <div className='grow'></div>
+                <Button className='justify-start' variant="ghost">
+                    <Icons.LogOut className="w-4 h-4 mr-2" />
+                    Wyloguj
+                </Button>
             </div>
         </div>  
     )
