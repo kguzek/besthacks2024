9192a97ce95424cbbcff11f325466b795a3c235a
Kamil Marczak <kamilm@you2.pl>
2024-10-13 05:02:40 +0200

    Update search.ts

--- twist/actions/search.ts
+++ twist/actions/search.ts
@@ -1,29 +1,44 @@
-"use server"
+'use server';
 
-import { openai } from "@ai-sdk/openai";
-import { embed } from "ai";
-import { MongoClient } from "mongodb"
+import { openai } from '@ai-sdk/openai';
+import { embed } from 'ai';
+import { MongoClient } from 'mongodb';
 
 async function findSimilarDocuments(embedding: number[]) {
     const client = new MongoClient(process.env.DATABASE_URL!);
 
     try {
         await client.connect();
-        
+
         const db = client.db('twist'); // Replace with your database name.
         const collection = db.collection('User'); // Replace with your collection name.
-        
+
         // Query for similar documents.
-        const documents = await collection.aggregate([
-        {"$vectorSearch": {
-            "queryVector": embedding,
-            "path": "skillsEmbedding",
-            "numCandidates": 100,
-            "limit": 5,
-            "index": "skills_vector_index",
-            }}
-        ]).toArray();
-        
+        const documents = await collection
+            .aggregate([
+                {
+                    $vectorSearch: {
+                        queryVector: embedding,
+                        path: 'skillsEmbedding',
+                        numCandidates: 100,
+                        limit: 5,
+                        index: 'skills_vector_index',
+                    },
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        name: 1,
+                        skills: 1,
+                        jobTitle: 1,
+                        score: {
+                            $meta: 'vectorSearchScore',
+                        },
+                    }
+                }
+            ])
+            .toArray();
+
         return documents;
     } finally {
         await client.close();
@@ -31,14 +46,15 @@ async function findSimilarDocuments(embedding: number[]) {
 }
 
 export const searchTest = async () => {
-    console.log("searching for offers");
-    const input = "HTML, CSS, JavaScript, React, Node.js, Express.js, RESTful APIs, Database Management (SQL/NoSQL), Version Control (Git), Responsive Design, UI/UX Principles, Testing and Debugging, Deployment and Hosting, Agile Methodologies, Problem Solving, Communication Skills, Collaboration Tools (e.g., Slack, Trello), Security Best Practices, Performance Optimization, Web Accessibility (a11y)"
+    console.log('searching for offers');
+    const input =
+        'HTML, CSS, JavaScript, React, Node.js, Express.js, RESTful APIs, Database Management (SQL/NoSQL), Version Control (Git), Responsive Design, UI/UX Principles, Testing and Debugging, Deployment and Hosting, Agile Methodologies, Problem Solving, Communication Skills, Collaboration Tools (e.g., Slack, Trello), Security Best Practices, Performance Optimization, Web Accessibility (a11y)';
 
     const { embedding } = await embed({
-        model: openai.embedding("text-embedding-3-small"),
+        model: openai.embedding('text-embedding-3-small'),
         value: input,
     });
 
     const documents = await findSimilarDocuments(embedding);
     console.log(documents);
-}
+};
